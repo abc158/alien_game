8b5ddeab955f70407d7ee1072427f1fd82a6e591
tanweiqin <weiqin.qin@amicro.com.cn>
2018-03-29 19:15:09 +0800

    增加方向处理

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,7 +14,7 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     #  ¥¥Ω®“ªÀ“∑…¥¨
-    ship = Ship(screen)
+    ship = Ship(ai_settings,screen)
     while True:
         gf.check_events(ship)
         gf.update_screen(ai_settings, screen, ship)
--- game_functions.py
+++ game_functions.py
@@ -9,13 +9,9 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-            # œÚ”““∆∂Ø∑…¥¨
-                ship.moving_right = True
+            check_keydown_events(event,ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-            # œÚ”““∆∂Ø∑…¥¨
-                ship.moving_right = False
+            check_keyup_events(event,ship)
 
 
 def update_screen(ai_settings, screen, ship):
@@ -24,5 +20,21 @@ def update_screen(ai_settings, screen, ship):
         ship.update()
         pygame.display.flip()
         
+def check_keydown_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+            # œÚ”““∆∂Ø∑…¥¨
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+    # œÚ”““∆∂Ø∑…¥¨
+        ship.moving_left = True
+                
+def check_keyup_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+            # œÚ”““∆∂Ø∑…¥¨
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+    # œÚ”““∆∂Ø∑…¥¨
+        ship.moving_left = False
+
 
 
